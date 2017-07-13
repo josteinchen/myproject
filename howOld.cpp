6a990369afc3532fba0a66cbf456d1ac9416e126
jostein <jostein.chen@gmail.com>
2017-07-13 19:05:40 +0800

    add my sourcecode

---
+++ howOld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int years;
+	cout << "How old are you?" << endl;
+	cin >> years;
+	cout << sizeof(years) << endl;
+	cout << "I am " << years << " years old." << endl;
+	system("pause");
+}
